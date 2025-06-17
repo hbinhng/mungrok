c1f7acab29021572354bfe0b6cd59bef2b8403e7
hbinhng <me@hbinhng.dev>
2025-06-17 07:26:27 +0000

    Update package, tool config, add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,34 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es2021: true,
-    },
-    extends: ['airbnb-base'],
-    parserOptions: {
-        ecmaVersion: 12,
-    },
-    rules: {
-        linebreaks: 'crlf',
-    },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./packages/*/tsconfig.json'],
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+  },
+  ignorePatterns: ['.eslintrc.js', '/dist/'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
 };
